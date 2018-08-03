202c76bbfc29ac25e2338a2b1a4e95f946bc794c
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-03 05:07:52 +0000

    added function to display who has most followers, and refactored the code for displaying who follows the most to accept multiple users with same number of follows

--- socialNetworking.js
+++ socialNetworking.js
@@ -32,12 +32,11 @@ var data = {
 };
 
 /**********************************************************************
- * DISPLAYFOLLOWINFO: displays follows & followers info in the format:
+ * displayFollowInfo: displays follows & followers info in the format:
  * Alice follows Bob, Charlie, Debbie.
  * Alice's followers are: Charlie, Debbie
  **********************************************************************/
 function displayFollowInfo(){
-    var keyArray = Object.keys(data);
     for(person in data){
         var followsMSG = data[person].name + " follows ";
         var follows = getFollows(person).join(", ");
@@ -51,7 +50,7 @@ function displayFollowInfo(){
 }
 
 /**********************************************************************
- * GETFOLLOWS: takes a person and returns an array of the names of 
+ * getFollows: takes a person and returns an array of the names of 
  * the people that person follows
  **********************************************************************/
 function getFollows(person){
@@ -64,7 +63,7 @@ function getFollows(person){
 }
 
 /**********************************************************************
- * GETFOLLOWERS: takes a person and returns an array of the names of 
+ * getFollowers: takes a person and returns an array of the names of 
  * the people that follow that person
  **********************************************************************/
 function getFollowers(person) {
@@ -78,5 +77,84 @@ function getFollowers(person) {
     return followers;
 }
 
+/****************************************************************************
+ * displayBiggestStalker: displays who follows the most people in the format:
+ * Debbie follows the most people.  She is a stalker.
+ ****************************************************************************/
+function displayBiggestStalker(){
+    var mostFollowsArray = [];
+    var highestNum = 0;
 
-displayFollowInfo();
+
+    for(person in data){
+        var mostFollows = {
+            name: "",
+            followCount: 0
+        };
+        var followsCount = getFollows(person).length;
+        mostFollows.name = data[person].name;
+        mostFollows.followCount = followsCount;
+        mostFollowsArray.push(mostFollows);
+        if (followsCount >= highestNum) {
+            highestNum = followsCount;
+        }
+    }
+
+    var multipleMost = [];
+    for (user in mostFollowsArray) {
+        if (mostFollowsArray[user].followCount == highestNum) {
+            multipleMost.push(mostFollowsArray[user].name);
+        }
+    }
+
+    var peopleString = multipleMost.join(", ");
+    if (multipleMost.length == 1) {
+        console.log(peopleString, "follows the most people.", peopleString, "is a stalker.");
+    } else {
+        console.log(peopleString, "follow the most people.", peopleString, "are a bunch of stalkers.");
+    }
+
+}
+
+/*************************************************************************************
+ * displayNeedsRestrainingOrder: displays who's being followed the most in the format:
+ * Debbie has the most followers.  Debbie should consider some restraining orders.
+ *************************************************************************************/
+function displayNeedsRestrainingOrder(){
+    var mostFollowersArray = [];
+    var highestNum = 0;
+
+    for(person in data){
+        var mostFollowers = {
+            name: "",
+            followersCount: 0
+        };
+
+        var personName = data[person].name;
+        var followersCount = getFollowers(person).length;
+        mostFollowers.name = personName;
+        mostFollowers.followersCount = followersCount;
+        mostFollowersArray.push(mostFollowers);
+        highestNum = followersCount;
+    }
+
+    var multipleMost = [];
+    
+    for (user in mostFollowersArray) {
+        if (mostFollowersArray[user].followersCount == highestNum) {
+            multipleMost.push(mostFollowersArray[user].name);
+        }
+    }
+
+    var peopleString = multipleMost.join(", ");
+    if (multipleMost.length > 1) {
+        console.log(peopleString, "have the most followers. They should consider some restraining orders.");
+    } else {
+        console.log(peopleString, "has the most followers.", peopleString, "should consider some restraining orders.");
+    }
+}
+
+
+// displayFollowInfo();
+// displayBiggestStalker();
+displayNeedsRestrainingOrder();
